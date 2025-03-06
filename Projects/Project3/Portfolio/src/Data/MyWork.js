67d70ec402de59433ee29a2acda3f2cd89104f98
Rupali Prabha <prabharupali@gmail.com>
2025-03-06 12:46:29 +0530

    Add Portfolio

--- Projects/Project3/Portfolio/src/Data/MyWork.js
+++ Projects/Project3/Portfolio/src/Data/MyWork.js
@@ -1,47 +1,46 @@
-import project1 from '../assets/project1.png'
-import project2 from '../assets/project2.png'
-import project3 from '../assets/project3.png'
-import project4 from '../assets/project4.png'
-import project5 from '../assets/project5.png'
-import project6 from '../assets/project6.png'
+import project1 from "../assets/project1.png";
+import project6 from "../assets/project6.png";
 
-  const mywork_data=[
-
-    {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1,
-        w_link:"https://innomatics-project1.netlify.app/"
-    },
-    {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2,
-        w_link:"https://innomatics-assingment-2.netlify.app/"
-    },
-    {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3,
-        w_link:"https://memory-magic-game-project2.netlify.app/"
-    },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4,
-        w_link:"https://tech-shopping-cart-2.netlify.app/"
-    },
-    {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5,
-        w_link:"https://movie-mylist.netlify.app/"
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6,
-        w_link:"https://habot-assingment.netlify.app/"
-    },
-]
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: "Web design",
+    w_img: project1,
+    w_link: "https://innomatics-project1.netlify.app/",
+  },
+  {
+    w_no: 2,
+    w_name: "Web design",
+    w_img:
+      "https://images.unsplash.com/photo-1718469045516-76fd00e975bf?q=80&w=2152&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    w_link: "https://innomatics-assingment-2.netlify.app/",
+  },
+  {
+    w_no: 3,
+    w_name: "Web design",
+    w_img:
+      "https://images.unsplash.com/photo-1611996575749-79a3a250f948?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGdhbWV8ZW58MHx8MHx8fDA%3D",
+    w_link: "https://memory-magic-game-project2.netlify.app/",
+  },
+  {
+    w_no: 4,
+    w_name: "Web design",
+    w_img:
+      "https://media.istockphoto.com/id/1215812761/photo/internet-shopping-with-laptop.webp?a=1&b=1&s=612x612&w=0&k=20&c=KE0mwpkxoM8moSyOD4nzQX5yLdr1oGi-mr36E-h38UI=",
+    w_link: "https://tech-shopping-cart-2.netlify.app/",
+  },
+  {
+    w_no: 5,
+    w_name: "Web design",
+    w_img:
+      "https://media.istockphoto.com/id/1194783078/photo/social-networking-service-concept-streaming-video-video-library.webp?a=1&b=1&s=612x612&w=0&k=20&c=MaZtSWEnOSL8RwHO_oK7PUe3PDuJmWLVbhvWVUTSWZU=",
+    w_link: "https://movie-mylist.netlify.app/",
+  },
+  {
+    w_no: 6,
+    w_name: "Web design",
+    w_img: project6,
+    w_link: "https://habot-assingment.netlify.app/",
+  },
+];
 export default mywork_data;
